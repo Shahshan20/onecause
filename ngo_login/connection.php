dda125406bebf87bb5c7c7dd8af0f1ed9032cd24
Shahshan20 <104528639+Shahshan20@users.noreply.github.com>
2023-11-20 23:24:07 +0500

    Update connection.php

--- ngo_login/connection.php
+++ ngo_login/connection.php
@@ -1,3 +1,3 @@
 <?php 
-$con = mysqli_connect('localhost', 'root', '', 'one_cause');
+$con = mysqli_connect('donateto1cause-server.mysql.database.azure.com', 'btufyinlhl', 'QB8SFZNTEGKCMK26$', 'one_cause');
 ?>
